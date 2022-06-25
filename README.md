52d5e7401860e0aced54cf7fb415e2358b5fec08
Floris Groenendijk <weirdall@hotmail.com>
2022-06-25 19:44:14 +0200

    Write to vga

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 part 1: https://os.phil-opp.com/freestanding-rust-binary/#summary - done
 
-part 2: https://os.phil-opp.com/minimal-rust-kernel/#the-build-std-option
+part 2: https://os.phil-opp.com/minimal-rust-kernel/#booting-it-in-qemu
+
+part 3: https://os.phil-opp.com/vga-text-mode/
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,13 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+/*    let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
@@ -24,6 +26,9 @@ pub extern "C" fn _start() -> ! {
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+*/
+
+    vga_buffer::print_something();
 
     loop{}
 }
